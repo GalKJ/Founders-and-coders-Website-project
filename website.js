e733dd2e652fc67b85b1939cf5b4cc53b0b6baaf
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-07-28 12:11:35 +0100

    add animation rules .css paragraph variables, logic for timed animation .js
    add keyframes, animation rules to paragraph elements in .css paragraph variables, for each loop, classlist.add and logic for setTimeout .js

--- website.js
+++ website.js
@@ -1,8 +1,29 @@
 // Section one
 
 // Select Elements
+// Blob buttons  
 const blobButtonOne = document.querySelector('#blob-button-one');
 const blobOne = document.querySelector('#radius1');
+// Introduction paragraphs
+const paragraphsOneThree = document.querySelectorAll('.paragraphs-one-three');
+const paragraphTwo = document.querySelector('#intro-paragraph-two');
+
+// Timers to call introSlide functions 
+setTimeout(introSlideRL, 750);
+setTimeout(introSlideLR, 750);
+
+// Function to add class to intro paragraph 
+function introSlideRL() {
+    paragraphsOneThree.forEach(para => {
+        para.style.visibility = "visible";
+        para.classList.add("slide-r-l");
+    });
+}
+
+function introSlideLR() {
+    paragraphTwo.style.visibility = "visible";
+    paragraphTwo.classList.add("slide-l-r");
+}
 
 // Add event listener to blob button with logic to toggle visibility of element
 blobButtonOne.addEventListener('click', () => {
