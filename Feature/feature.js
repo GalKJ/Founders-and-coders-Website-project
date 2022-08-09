1a5fd3751678bb15b901335bc5c5a35d7c7b1639
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-08-09 08:58:42 +0100

    add list item update .html etch-a-sketch canvas lineTo and stroke to clear canvas function .js

--- Feature/feature.js
+++ Feature/feature.js
@@ -81,9 +81,11 @@ function clearCanvas() {
     { once: true }
   );
   ctx.beginPath(); // start the drawing again at a random point in state
-    x = Math.floor(Math.random() * width);
-    y = Math.floor(Math.random() * height);
-    ctx.moveTo(x, y);
+  x = Math.floor(Math.random() * width);
+  y = Math.floor(Math.random() * height);
+  ctx.moveTo(x, y);
+  ctx.lineTo(x, y);
+  ctx.stroke();
 }
 
 // Listen for arrow keys
